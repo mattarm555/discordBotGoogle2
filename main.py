b8c22672fc274034b5d8da23bc606f2da115ce3c
ryanhur51 <ryanhur51@gmail.com>
2025-12-24 13:46:47 -0500

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
 from datetime import datetime
-
+ 
 # Load environment variables
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
